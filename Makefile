458b2e7f7cb120e59ad2a1defe4aef0c1933a3c0
Reicher <robin.o.reicher@gmail.com>
2013-03-10 19:43:40 +0100

    Added Client, not tested at all

--- Makefile
+++ Makefile
@@ -3,13 +3,15 @@ CC=g++
 CFLAGS=-I/usr/local/include -I/include -I/src -c 
 LFLAGS=-L/usr/local/lib -lwiringPi -o
 
-all: main.o DCMotor.o Server.o
-	$(CC) $(LFLAGS) Plancobile main.o DCMotor.o Server.o 
+all: main.o DCMotor.o Server.o Client.o
+	$(CC) $(LFLAGS) Plancobile main.o DCMotor.o Server.o Client.o 
 main.o: src/main.cpp
 	$(CC) $(CFLAGS) src/main.cpp
 DCMotor.o: src/DCMotor.cpp include/DCMotor.h
 	$(CC) $(CFLAGS) src/DCMotor.cpp
 Server.o: src/Server.cpp include/Server.h
 	$(CC) $(CFLAGS) src/Server.cpp
+Client.o: src/Client.cpp include/Client.h
+	$(CC) $(CFLAGS) src/Client.cpp
 clean: 
 	rm -rf *.o Plancobile
---
+++ src/Client.cpp
@@ -0,0 +1,42 @@
+#include "../include/Client.h"
+
+Client::Client(const char *host, int portNum)
+  : m_portNum( portNum )
+{
+  m_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(m_sockfd < 0)
+    error("ERROR opening socket!");
+  
+  m_pServer = gethostbyname(host);
+  if(m_pServer == NULL)
+    {
+      error("ERROR no such host!");
+    }
+  bzero((char *) &m_serv_addr, sizeof(m_serv_addr));
+  m_serv_addr.sin_family = AF_INET;
+  bcopy((char *)m_pServer->h_addr, (char *)&m_serv_addr.sin_addr.s_addr, m_pServer->h_length);
+  m_serv_addr.sin_port = htons(m_portNum);
+  if(connect(m_sockfd, (struct sockaddr *) &m_serv_addr, sizeof(m_serv_addr)) < 0)
+    error("ERROR connecting!");
+
+  cout << "Enter message: ";
+  bzero(m_buffer, 256);
+  fgets(m_buffer, 255, stdin);
+  m_n = write(m_sockfd, m_buffer, strlen(m_buffer));
+  if(m_n < 0)
+    error("ERROR writing to socket!");
+
+  bzero(m_buffer, 256);
+  m_n = read(m_sockfd, m_buffer, 255);
+  if(m_n < 0)
+    error("ERRPR readomg from socket!");
+
+  cout << m_buffer;
+  close(m_sockfd);
+}
+
+void Client::error(const char *msg)
+{
+  perror(msg);
+  exit(0);
+}
