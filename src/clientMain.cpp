4297aecc780cba5903c5cc18d97df9bc1d456517
Reicher <robin.o.reicher@gmail.com>
2013-03-31 21:03:28 +0200

    fixed broken clientMain

--- src/clientMain.cpp
+++ src/clientMain.cpp
@@ -2,20 +2,11 @@
 
 int main(int argc, char* argv[])
 {
-<<<<<<< HEAD
-	Client* client;
-	if(argc > 1)
-		client = new Client(argv[1], 51717);
-	else
-		client = new Client("localhost", 51717);
-	
-=======
   Client* client;
   if(argc > 1)
     client = new Client(argv[1], 51717);
-  else 
+  else
     client = new Client("localhost", 51717);
-  
->>>>>>> 5548345afde83970f2fbb9952aac4556e2318e45
+
   return 0;
 }
